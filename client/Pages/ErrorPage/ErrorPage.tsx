dca3c6046f965c931d5f8cd288c7c5d826d4daab
Rigelle <rigellecruz@gmail.com>
2023-06-21 16:19:26 +1200

    complete styling

--- client/Pages/ErrorPage/ErrorPage.tsx
+++ client/Pages/ErrorPage/ErrorPage.tsx
@@ -1,12 +1,20 @@
+import Header from '../../components/Header/Header'
+
 function ErrorPage() {
   return (
-    <div className="bg-darkPurple h-screen text-violet-400">
-      <img src="error-symbol.svg" alt="error symbol" />
-      <p className="text-sm">Something went wrong</p>
-      <button className="bg-white rounded-lg py-2 px-4 text-pink-500">
-        <a href="/">Home</a>
-      </button>
-    </div>
+    <>
+      <div className="bg-darkPurple h-screen text-violet-400 flex items-center justify-center flex-col gap-10">
+        <div className="text-5xl">
+          <i className="fa-2xl fa-solid fa-triangle-exclamation text-pink-500"></i>
+        </div>
+        <p className="text-sm">Something went wrong</p>
+        <button className="bg-white rounded-lg py-2 px-4 text-pink-500 w-20 hover:text-white hover:bg-pink-500 hover:shadow-[0px_0px_9px_2px_#FF17CE]">
+          <a className="font-bold" href="/">
+            Home
+          </a>
+        </button>
+      </div>
+    </>
   )
 }
 
