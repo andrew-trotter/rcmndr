c9837489fd707785a0b71e49c225a5a3eea34736
Rigelle <rigellecruz@gmail.com>
2023-06-22 10:07:32 +1200

    set up boilerplate post route for adding a song

--- server/routes/songs.ts
+++ server/routes/songs.ts
@@ -6,4 +6,13 @@ const router = express.Router()
 
 // POST /api/v1/songs
 
+router.post('/api/v1/songs', async (req, res) => {
+  try {
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: 'oops something went wrong' })
+    }
+  }
+})
+
 export default router
