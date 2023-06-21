4479c0b8b7024959c30130a876cb49b3b609af6d
Tem <tem.pearse01@gmail.com>
2023-06-21 16:50:42 +1200

    started authentication step for songs route

--- server/routes/songs.ts
+++ server/routes/songs.ts
@@ -1,8 +1,12 @@
 import express from 'express'
+import { validateAccessToken } from '../auth0'
+
+
 
 const router = express.Router()
 
 // GET /api/v1/songs
+router.get('/', validateAccessToken )
 
 // POST /api/v1/songs
 
