fedf3d68367abdf272984fccde5b031a80ef1b58
Rigelle <rigellecruz@gmail.com>
2023-06-21 15:04:09 +1200

    working on styling

--- client/Pages/ErrorPage/ErrorPage.stories.tsx
+++
@@ -1,5 +0,0 @@
-import type { Meta, StoryObj } from '@storybook/react'
-
-import ErrorPage from './ErrorPage'
-import Background from '../../components/UI/Background/Background'
-
--- client/Pages/ErrorPage/ErrorPage.tsx
+++ client/Pages/ErrorPage/ErrorPage.tsx
@@ -1,8 +1,11 @@
 function ErrorPage() {
   return (
-    <div>
-      <p>ERROR!</p>
-      <a href="/">Back to Home</a>
+    <div className="bg-darkPurple h-screen text-violet-400">
+      <img src="error-symbol.svg" alt="error symbol" />
+      <p className="text-sm">Something went wrong</p>
+      <button className="bg-white rounded-lg py-2 px-4 text-pink-500">
+        <a href="/">Home</a>
+      </button>
     </div>
   )
 }
