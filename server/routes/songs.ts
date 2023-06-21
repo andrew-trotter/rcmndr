37ba974d22eff42ad234b0f421bc4fefe56906b9
Rigelle <rigellecruz@gmail.com>
2023-06-22 11:02:19 +1200

    wip

--- server/routes/songs.ts
+++ server/routes/songs.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { songDraftSchema } from '../../types/Song'
+import * as usersDb from '../db/users'
 
 const router = express.Router()
 
@@ -7,11 +8,13 @@ const router = express.Router()
 
 // POST /api/v1/songs
 
-router.post('/api/v1/songs', async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const input = req.body
     const songData = songDraftSchema.parse(input)
-    res.json(songData)
+    console.log(songData)
+    usersDb.insertSong(songData)
+    res.sendStatus(201)
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ error: 'oops something went wrong' })
