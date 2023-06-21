5385cc0d9a08a41cf84c518a732481d3a629bc60
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-22 07:15:21 +1200

    serve assests

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { join } from 'path'
+import { join, resolve } from 'path'
 
 import userRouter from './routes/users'
 import songRouter from './routes/songs'
@@ -8,13 +8,16 @@ import notifications from './routes/notifications'
 const server = express()
 
 server.use(express.json())
-
+server.use(express.static(join(__dirname, 'public')))
 server.use('/api/v1/users', userRouter)
 server.use('/api/v1/songs', songRouter)
 server.use('/api/v1/notifications', notifications)
 
-server.get('*', (_, res) => {
-  res.sendFile(join(__dirname, '../index.html'))
-})
+if (process.env.NODE_ENV === 'production') {
+  server.use('/assets', express.static(resolve(__dirname, '../dist/assets')))
+  server.get('*', (_, res) => {
+    res.sendFile(join('public', 'index.html'))
+  })
+}
 
 export default server
