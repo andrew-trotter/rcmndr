8bd42f3587557dffaa9ab31e3cca3569daac3589
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-22 07:13:12 +1200

    serve index.html for deployment

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { join } from 'path'
 
 import userRouter from './routes/users'
 import songRouter from './routes/songs'
@@ -11,5 +12,9 @@ server.use(express.json())
 server.use('/api/v1/users', userRouter)
 server.use('/api/v1/songs', songRouter)
 server.use('/api/v1/notifications', notifications)
+
+server.get('*', (_, res) => {
+  res.sendFile(join(__dirname, '../index.html'))
+})
 
 export default server
