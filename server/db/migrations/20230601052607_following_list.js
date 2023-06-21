c98f9b8fd79047779fdd64a09a27a73411182b8f
Sarah <sarahchungwu@gmail.com>
2023-06-21 15:15:28 +1200

    change the index name for the following_list

--- server/db/migrations/20230601052607_following_list.js
+++ server/db/migrations/20230601052607_following_list.js
@@ -4,7 +4,7 @@ exports.up = async function (knex) {
     table.primary(['user_id', 'following_id'])
     table.string('user_id').references('users.auth0_id').notNullable()
     table.string('following_id').references('users.auth0_id').notNullable()
-    table.index(['user_id'], 'idx_user_id')
+    table.index(['user_id'], 'idx_following_list_user_id')
   })
 }
 
