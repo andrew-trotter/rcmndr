37da9f2b9dee6a2690154f0a25f3060ae2c5668c
Jane <jiejanezhang2012@gmail.com>
2023-06-23 10:00:22 +1200

    deleted the spinner code & refactored slogan animation code

--- client/components/UI/Slogan/Slogan.tsx
+++ client/components/UI/Slogan/Slogan.tsx
@@ -2,32 +2,20 @@ import { twMerge } from 'tailwind-merge'
 import { motion } from 'framer-motion'
 
 function Slogan() {
+  const texts = ['collate.', 'recommend.', 'discover.']
   return (
     <div className={twMerge('text-white')}>
-      <motion.p
-        initial={{ opacity: 0, translateY: -50 }}
-        animate={{ opacity: 1, translateY: 0 }}
-        transition={{ duration: 1, delay: 0.5 }}
-        className="text-4xl"
-      >
-        collate.
-      </motion.p>
-      <motion.p
-        initial={{ opacity: 0, translateY: -50 }}
-        animate={{ opacity: 1, translateY: 0 }}
-        transition={{ duration: 1, delay: 1.6 }}
-        className="text-4xl"
-      >
-        recommend.
-      </motion.p>
-      <motion.p
-        initial={{ opacity: 0, translateY: -50 }}
-        animate={{ opacity: 1, translateY: 0 }}
-        transition={{ duration: 1, delay: 2.8 }}
-        className="text-4xl"
-      >
-        discover.
-      </motion.p>
+      {texts.map((text, index) => (
+        <motion.p
+          initial={{ opacity: 0, translateY: -50 }}
+          animate={{ opacity: 1, translateY: 0 }}
+          transition={{ duration: 1, delay: 0.5 + index * 1.1 }}
+          className="text-4xl"
+          key={text}
+        >
+          {text}
+        </motion.p>
+      ))}
     </div>
   )
 }
