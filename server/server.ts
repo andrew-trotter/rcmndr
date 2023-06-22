8bcda502b675bb58c9e6eefec65f47ab8f03f3be
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-22 13:35:47 +1200

    compress all responses

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import compress from 'compression'
 
 import userRouter from './routes/users'
 import songRouter from './routes/songs'
@@ -7,6 +8,7 @@ import notifications from './routes/notifications'
 const server = express()
 
 server.use(express.json())
+server.use(compress())
 
 server.use('/api/v1/users', userRouter)
 server.use('/api/v1/songs', songRouter)
