5d22ce3face2c9b7770111135630c3ecb0c4c2c7
Daniel <danielgrabczewski@gmail.com>
2023-06-23 11:30:50 +1200

    add logic to AddSong form for backend route

--- client/Pages/AddSong/Songs.tsx
+++ client/Pages/AddSong/Songs.tsx
@@ -1,11 +1,51 @@
 import AddSong from "../../components/AddSong/AddSong"
 import { insertSong } from "../../apis/songs"
+import { SongDraft } from "../../../types/Song"
+
+import { useAuth0 } from '@auth0/auth0-react'
+import { useNavigate } from 'react-router-dom'
+import { useMutation } from 'react-query'
+
 
 function Songs () {
 
+    //Pass in a handlesubmit prop, which takes the form data.
+    //When it is run, put the data into the api.
+  const navigate = useNavigate()
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
+    useAuth0()
+
+    const mutation = useMutation({
+      mutationFn: ({
+        form,
+        token,
+      }: {
+        form: SongDraft
+        token: string
+      }) => insertSong(form, token),
+      onSuccess: () => {
+        navigate('/my-songs')
+      },
+    })
+
+    async function handleSubmit(form: SongDraft) {
+      const token = await getAccessTokenSilently()
+      mutation.mutate({ form, token })
+      navigate('/my-songs')
+    }
+
+
+    if (isLoading) {
+      return <div>Loading ...</div>
+    }
+  
+    if (!isAuthenticated && !user) {
+      return <div>Not authenticated</div>
+    }
+
 
 return (
-  <AddSong  />
+  <AddSong handleSubmit={handleSubmit} />
 )
 }
 
--- client/components/AddSong/AddSong.tsx
+++ client/components/AddSong/AddSong.tsx
@@ -2,10 +2,34 @@ import { Song, SongDraft } from '../../../types/Song'
 import Button from '../UI/Button/Button'
 import TextBox from '../UI/TextBox/TextBox'
 
-function AddSong() {
+interface Props {
+  handleSubmit : (form : SongDraft) => void
+}
+
+function AddSong(props : Props) {
+  function handleSubmit (e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const title = formData.get('title') as string
+    const artist = formData.get('artist') as string
+    const genre = formData.get('genre') as string | null
+    const link = formData.get('genre') as string | null
+    const comment =  formData.get('comment') as string
+    
+    const form = {
+      title : title,
+      artist : artist,
+      genre : genre,
+      link : link,
+      comment : comment
+    }
+
+    props.handleSubmit(form)
+  }
+
   return (
     <div>
-      <form /*onSubmit={handleSubmit}*/ className="space-y-4 p-4 text-white">
+      <form onSubmit={handleSubmit} className="space-y-4 p-4 text-white">
         <div className="space-y-2">
           <label htmlFor="title">Song title *</label>
           <TextBox
