90fc6e6e93a51979570b419c509bfcda850732f8
Jane Zhang <jiejanezhang2012@gmail.com>
2023-06-23 12:20:16 +1200

    the ScanQR worked

--- client/Pages/ScanQR/ScanQR.tsx
+++ client/Pages/ScanQR/ScanQR.tsx
@@ -1,5 +1,36 @@
+
+
+import { useState } from 'react';
+import { OnResultFunction, QrReader } from 'react-qr-reader';
+
+
+
+
 function ScanQR() {
-  return <p>I am in the ScanQR Page</p>
+  const [data, setData] = useState<string | null>(null)
+
+  const handleResult: OnResultFunction = (result, error) => {
+    if (result) {
+      setData(() => result.getText());
+      console.log(result.getText())
+      // Navigate('/confirm/auth0|2323432')
+      return
+    }
+    console.error(error);
+  }
+
+  return (
+    <>
+      <QrReader
+        constraints={{ width: 100 }}
+        onResult={handleResult}
+      />
+      <p>{data}</p>
+    </>
+  );
 }
 
-export default ScanQR
+export default ScanQR;
+
+
+
