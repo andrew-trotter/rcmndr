6559b017f2a9150a5311dffb4bc6601646eeaebf
Sarah <sarahchungwu@gmail.com>
2023-06-26 12:00:50 +1200

    get the qr code work in the show qr component

--- client/Pages/ShowQR/ShowQR.tsx
+++ client/Pages/ShowQR/ShowQR.tsx
@@ -1,5 +1,19 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
 function ShowQR() {
-  return <p>I am the ShowQR</p>
+  const { user } = useAuth0()
+
+  return (
+    <div>
+      <p>Hi,{user?.nickname}</p>
+
+      <p>Here is your QR code</p>
+      <img
+        src={`https://api.qrserver.com/v1/create-qr-code/?data=${user?.sub}&amp;size=100x100`}
+        alt=""
+      />
+    </div>
+  )
 }
 
 export default ShowQR
